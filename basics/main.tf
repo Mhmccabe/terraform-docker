916a1b7cc2d20e5289f4b00e82f4dc15f329d122
mhmccabe <m_h_mccabe@hotmail.com>
2020-04-17 11:54:43 +0100

    split into multiple files

--- basics/main.tf
+++ basics/main.tf
@@ -8,32 +8,6 @@ provider "docker" {
 }
 
 
-##############################
-# Define variables
-##############################
-variable "image_name" {
-  description = "Image for container."
-  default     = "ghost:latest"
-}
-
-variable "container_name" {
-  description = "Name of the container."
-  default     = "blog"
-}
-
-variable "int_port" {
-  description = "Internal port for container."
-  default     = "2368"
-}
-
-variable "ext_port" {
-  description = "External port for container."
-  default     = "80"
-}
-
-
-
-
 ###################################
 # Download the latest Ghost image
 ###################################
@@ -51,20 +25,4 @@ resource "docker_container" "container_id" {
     internal = "${var.int_port}"
     external = "${var.ext_port}"
   }
-}
-
-###########################################
-# Output the IP Address of the Container
-###########################################
-output "ip_address" {
-  value       = "${docker_container.container_id.ip_address}"
-  description = "The IP for the container."
-}
-
-####################################
-# Output the Name of the Container
-####################################
-output "container_name" {
-  value       = "${docker_container.container_id.name}"
-  description = "The name of the container."
 }
