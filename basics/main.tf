d00e72b6fdd8bb25630418224c85d37bea51cf79
mhmccabe <m_h_mccabe@hotmail.com>
2020-04-17 11:45:01 +0100

    added comments

--- basics/main.tf
+++ basics/main.tf
@@ -1,13 +1,16 @@
+#####################################################################
 # configure the docker client for terraform
 # remember to enable the TLS port on the docker engine settings
+#####################################################################
 provider "docker" {
   version = "~> 2.2"
   host = "tcp://localhost:2375"
 }
 
 
-#########################################################################
-#Define variables
+##############################
+# Define variables
+##############################
 variable "image_name" {
   description = "Image for container."
   default     = "ghost:latest"
@@ -31,13 +34,16 @@ variable "ext_port" {
 
 
 
-#########################################################################
+###################################
 # Download the latest Ghost image
+###################################
 resource "docker_image" "image_id" {
   name = "${var.image_name}"
 }
 
+#######################
 # Start the Container
+#######################
 resource "docker_container" "container_id" {
   name  = "${var.container_name}"
   image = "${docker_image.image_id.latest}"
@@ -47,13 +53,17 @@ resource "docker_container" "container_id" {
   }
 }
 
-#Output the IP Address of the Container
+###########################################
+# Output the IP Address of the Container
+###########################################
 output "ip_address" {
   value       = "${docker_container.container_id.ip_address}"
   description = "The IP for the container."
 }
 
-#Output the Name of the Container
+####################################
+# Output the Name of the Container
+####################################
 output "container_name" {
   value       = "${docker_container.container_id.name}"
   description = "The name of the container."
