9de0aae9325366bf4dffc6e89124f99a95430da2
mhmccabe <m_h_mccabe@hotmail.com>
2020-04-17 11:42:19 +0100

    added parameters to the file

--- basics/main.tf
+++ basics/main.tf
@@ -1,8 +1,37 @@
+# configure the docker client for terraform
+# remember to enable the TLS port on the docker engine settings
 provider "docker" {
   version = "~> 2.2"
   host = "tcp://localhost:2375"
 }
 
+
+
+#Define variables
+variable "image_name" {
+  description = "Image for container."
+  default     = "ghost:latest"
+}
+
+variable "container_name" {
+  description = "Name of the container."
+  default     = "blog"
+}
+
+variable "int_port" {
+  description = "Internal port for container."
+  default     = "2368"
+}
+
+variable "ext_port" {
+  description = "External port for container."
+  default     = "80"
+}
+
+
+
+
+# Download the latest Ghost image
 resource "docker_image" "image_id" {
   name = "ghost:latest"
 }
